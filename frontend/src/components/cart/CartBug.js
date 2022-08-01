dce68434b683e52e67e4406677eef68efc3b14e4
luminouspear <61656433+luminouspear@users.noreply.github.com>
2022-08-01 09:42:50 -0400

    frontend design v1 done

--- frontend/src/components/cart/CartBug.js
+++ frontend/src/components/cart/CartBug.js
@@ -12,12 +12,12 @@ export default function CartBug() {
                 <span className="text-center">
                     <KStoreTitle title="your order" textType="nocaps" />
                 </span>
-                <ul className="flex flex-col mt-12 px-6 lg:px-0 space-y-6 list-none w-10/12 lg:w-full mx-auto">
+                <ul className="flex flex-col mt-12 px-6 lg:px-0 space-y-6 list-none w-full mx-auto">
                     <li className=" font-quicksand text-white lg:text-kblack w-full">
                         <div className="flex flex-row justify-between">
                             <span className="text-xl">Subtotal</span>
 
-                            <span className="text-2xl font-medium">CA$25.00</span>
+                            <span className="text-2xl font-medium">CA$50.00</span>
                         </div>
                     </li>
                     <li className=" font-quicksand text-white lg:text-kblack w-full">
@@ -31,7 +31,7 @@ export default function CartBug() {
                         <div className="flex flex-row justify-between items-end">
                             <span className="text-xl font-bold">Estimated Total</span>
 
-                            <span className="text-3xl font-bold">CA$50.00</span>
+                            <span className="text-3xl font-bold">CA$75.00</span>
                         </div>
                     </li>
                 </ul>
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import CartBug from '../components/cart/CartBug';
 import CartItem from '../components/cart/CartItem';
 import Footer from '../components/global/footer_elements/Footer'
+import { KStoreTitle } from '../components/global/userinterface/KStoreTitle';
 
 
 
@@ -12,9 +13,11 @@ export default class CartScreen extends Component {
 
     return (
       <div className="w-full bg-[#111] mt-4 pt-6 pb-5">
-        <div className="w-full max-w-7xl container mx-auto bg-transparent mt-6 md:mt-12 mb-12 ">
+        <div className="w-full max-w-7xl container mx-auto bg-transparent mt-6 md:mt-12 mb-12 min-h-[95%] ">
           <div className="flex lg:flex-row flex-col w-full">
             <div className="basis-full lg:basis-9/12  mx-auto w-full flex flex-col">
+              <div className="w-full px-6 mb-6 text-center lg:text-left">
+                <KStoreTitle title="your cart" textType="sectionheader" /></div>
               <CartItem />
               <CartItem />
             </div>
